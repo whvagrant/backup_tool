078f52ff1166cd3432021c652298b6ba84bacc0c
whvagrant <whvagrant@163.com>
2016-06-20 20:36:51 +0800

    add configure file

---
+++ config.sh
@@ -0,0 +1,33 @@
+#######################################################
+# @desc configure file for backup_customised_files.sh
+# @date 2016/06/16
+#
+# @author wanghao46@baidu.com
+######################################################
+
+# default direcotries to bakcup
+all_dir=(
+    "sql"
+    "tmp"
+    "bin"
+)
+
+# HOME dir
+HOME=(
+    "/home/map/wanghao46"
+)
+
+# BACKUP dir
+BAK_DIR=(
+    "$HOME/bak"
+)
+
+# remote host
+remote_host=(
+    "wanghao46@cp01-rdqa-dev512.cp01.baidu.com"
+)
+
+# tar tool
+tar=(
+    "/home/map/.jumbo/bin/tar"
+)
